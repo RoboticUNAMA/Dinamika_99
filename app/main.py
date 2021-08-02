80f0a511cbcca7344ace1114e6c13649e4e4bc1a
Arjuna Panji <arjuna.panji97@gmail.com>
2021-08-02 15:54:34 +0700

    a

--- app/main.py
+++ app/main.py
@@ -300,20 +300,22 @@ def arahRobotDepan():
                 cv2.line(frame1, (int(cenX_ball), int(cenY_ball + 20)), (int(cenX_ball + 50), int(cenY_ball + 20)), [0,255,0], 2, 8)
                 cv2.putText(frame1, "Actual", (int(cenX_ball + 50), int(cenY_ball + 20)), font, 0.5, [0,255,0], 2)
                 
-                if cenX_ball < 150  :
+                if cenX_ball < 120  :
+                    # kiri robot
                     setMotor(motor,-30,-30,-30,-30)
                     
-                elif cenX_ball > 420  :
+                elif cenX_ball > 280  :
+                    # kanan robot
                     setMotor(motor,30,30,30,30)
                     
-                elif cenX_ball < 290  :
+                elif cenX_ball < 180  :
                     setMotor(motor,-35,-35,-35,-35)
                     sleep(0.1)
                     setMotor(motor,0,0,0,0)
                     dari = "kanan"
                     #print("PUTAR KANAN")
                 
-                elif cenX_ball > 320 :
+                elif cenX_ball > 220 :
                 
                     setMotor(motor,35,35,35,35)
                     sleep(0.1)
