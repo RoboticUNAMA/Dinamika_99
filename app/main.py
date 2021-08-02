db83d911b23984ce7f339eeb665cdc3a57886567
Arjuna Panji <arjuna.panji97@gmail.com>
2021-08-03 01:18:39 +0700

    debugging..

--- app/main.py
+++ app/main.py
@@ -525,7 +525,7 @@ def main():
     putarDerajat(87,0)
     sleep(0.1)
     setMotor(motor, 80,80,80,80) # putar kiri
-    sleep(0.2)
+    sleep(0.3)
     setMotor(motor, -50,-50,-50,-50) # rem putar kiri
     sleep(0.1)
     setMotor(motor, 0,0,0,0)
