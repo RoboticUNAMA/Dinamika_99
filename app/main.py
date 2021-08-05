2213e1cc582fe0a3c3bba2911da0cd4a7104a0d9
Arjuna Panji <arjuna.panji97@gmail.com>
2021-08-05 16:23:48 +0700

    mode 2

--- app/main.py
+++ app/main.py
@@ -766,11 +766,11 @@ def lurusBolaAtas():
         ballContours1, _ = cv2.findContours(BALL_MORPH1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         ballContours1 = sorted(ballContours1, key=lambda x:cv2.contourArea(x), reverse=True)
 
-        if state == "FINISH":
-            setMotor(motor,0,0,0,0)
-            motor.close()
-            cv2.destroyAllWindows()
-            break
+        # if state == "FINISH":
+        #     setMotor(motor,0,0,0,0)
+        #     motor.close()
+        #     cv2.destroyAllWindows()
+        #     break
 
         if db.isOpen() == False:
             db.open()
