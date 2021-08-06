9fb92fb1799c8be8a49ebb27479d1e3e19ceb932
Arjuna Panji <arjuna.panji97@gmail.com>
2021-08-06 18:13:39 +0700

    update

--- app/main.py
+++ app/main.py
@@ -682,7 +682,7 @@ def arahKiper():
                 cv2.line(frame1, (int(cenX_ball), int(cenY_ball + 20)), (int(cenX_ball + 50), int(cenY_ball + 20)), [0,255,0], 2, 8)
                 cv2.putText(frame1, "Actual", (int(cenX_ball + 50), int(cenY_ball + 20)), font, 0.5, [0,255,0], 2)
                 
-                if cenX_ball < 200  :
+                if cenX_ball < 230  :
                     setMotor(motor,-35,-35,-35,-35)
                     sleep(0.5)
                     setMotor(motor,35,35,35,35)
@@ -691,7 +691,7 @@ def arahKiper():
                     state = "FINISH"
                     print("KIPER KIRI")
                     
-                elif cenX_ball > 250  :
+                elif cenX_ball > 260  :
                     setMotor(motor,35,35,35,35)
                     sleep(0.5)
                     setMotor(motor,-35,-35,-35,-35)
@@ -700,7 +700,7 @@ def arahKiper():
                     state = "FINISH"
                     print("KIPER KANAN")
                     
-                elif cenX_ball > 200 and cenX_ball < 250  :
+                elif cenX_ball > 230 and cenX_ball < 260  :
                     setMotor(motor,-35,-35,-35,-35)
                     sleep(0.5)
                     setMotor(motor,35,35,35,35)
