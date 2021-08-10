7233b55ec4793d8050b93f38d309455503342282
Arjuna Panji <arjuna.panji97@gmail.com>
2021-08-10 19:07:36 +0700

    mode4

--- app/main.py
+++ app/main.py
@@ -1336,7 +1336,7 @@ def main():
         arahBolaDepan()
 
         setMotor(motor, -80,-80,-80,-80) # motor putar kanan
-        sleep(0.8)) 
+        sleep(0.8)
         setMotor(motor, 50,50,50,50) # rem putar kanan
         sleep(0.1)
         setMotor(motor, 0,0,0,0) # motor stop
