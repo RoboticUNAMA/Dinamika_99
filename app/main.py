ea30a16b3c3ad3f290e3fc1976c5a15816253ff2
Arjuna Panji <arjuna.panji97@gmail.com>
2021-08-12 15:03:00 +0700

    a

--- app/main.py
+++ app/main.py
@@ -552,10 +552,10 @@ def arahRobotDepan():
 
         if ada == 0:
             counter += 1
-            if counter > 10 and dari == "kanan":
+            if counter > 15 and dari == "kanan":
                 counter = 0
                 dari = "kiri"
-            elif counter > 10 and dari == "kiri":
+            elif counter > 15 and dari == "kiri":
                 counter = 0
                 dari = "kanan"
 
