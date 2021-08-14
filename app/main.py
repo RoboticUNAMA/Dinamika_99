fcac08d974625b3f5617f9da0d2064c42e072ffb
Arjuna Panji <arjuna.panji97@gmail.com>
2021-08-14 11:42:45 +0700

    new main program

--- app/main.py
+++ app/main.py
@@ -1172,7 +1172,7 @@ def main():
                 if mode == "KICKOFF KANAN":
                     maju(50, 0.5)
                     geserKiri(100, 2.3)
-                    maju(100, 2)
+                    maju(100, 1.5)
                     stop()
                     break
         elif gameStatus == "RETRY":
