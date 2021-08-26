726682d542671674fa114fc42999ee230456ed4b
Arjuna Panji <arjuna.panji97@gmail.com>
2021-08-26 14:51:11 +0700

    mode2

--- app/main.py
+++ app/main.py
@@ -1277,7 +1277,7 @@ def main():
                         mundurSerongKanan(180, 3)
                         #setGame("STOP")
                         break
-                    mundur(90, 1.6)
+                    mundur(90, 1.2)
                     putarKanan(90, 0.3)
                     gameStatus = arahRobotDepan()
                     if gameStatus == "RETRY":
