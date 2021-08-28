8e180c23541a2cf716e41c525ccf65e93be4a0fb
Arjuna Panji <arjuna.panji97@gmail.com>
2021-08-28 15:05:12 +0700

    limit sukses

--- app/main.py
+++ app/main.py
@@ -1278,7 +1278,7 @@ def lurusBolaAtas():
         if db.isOpen() == False:
             db.open()
         reading = db.readline().decode('utf-8','ignore')
-        db.reset_input_buffer()
+        # db.reset_input_buffer()
         if len(reading) > 0 :
             head = reading[0:5]
             print(head)
@@ -1308,10 +1308,10 @@ def lurusBolaAtas():
                     setMotor(motor,speed,speed,speed,speed)
                     
                 elif cenX_ball < 195 and cenY_ball < 100 :
-                    putarKanan(speed, 0.3)
+                    putarKanan(speed, 0.2)
                 
                 elif cenX_ball > 220 and cenY_ball < 100 :
-                    putarKiri(speed, 0.3)
+                    putarKiri(speed, 0.2)
                 else :
                     if cenY_ball < 90:
                         setMotor(motor,-50,50,-50,50)
@@ -1459,11 +1459,13 @@ def main():
                         if getStatus(1) == "READY":
                             break
                     oper(db)
+                    setStatus(2, "RUNNING")
                     serongKanan(180, 1.2)
                     putarKiri(90, 0.4)
                     gameStatus = arahBolaDepan()
                     if gameStatus == "RETRY":
                         dribbling(db,0)
+                        putarDerajat(86,0)
                         mundurSerongKanan(180, 1.5)
                         mundur(90, 1.5)
                         #setGame("STOP")
