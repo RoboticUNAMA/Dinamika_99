b9aa5945c5fc10d92fb291a5cf18790c8d2a29ef
Arjuna Panji <arjuna.panji97@gmail.com>
2021-08-28 14:55:23 +0700

    limit sukses

--- app/main.py
+++ app/main.py
@@ -1277,8 +1277,8 @@ def lurusBolaAtas():
 
         if db.isOpen() == False:
             db.open()
-        # db.reset_input_buffer()
         reading = db.readline().decode('utf-8','ignore')
+        db.reset_input_buffer()
         if len(reading) > 0 :
             head = reading[0:5]
             print(head)
