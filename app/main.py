c5eb4baeddd0be3b81303744705fc6b912e7831c
Arjuna Panji <arjuna.panji97@gmail.com>
2021-09-07 15:54:56 +0700

    mode3

--- app/main.py
+++ app/main.py
@@ -880,123 +880,149 @@ def arahKiper():
             break
 
 def mulaiSerongKiri():
+    print("==>> MULAI SERONG KIRI")
     # get center of the frame
-    _, frame1 = FRONT_CAP.read()
-    rows, cols, _ = frame1.shape
-    cenX_frame1 = int(cols/2)
-    cenY_frame1 = int(rows/2)
+    # _, frame1 = FRONT_CAP.read()
+    # rows, cols, _ = frame1.shape
+    # cenX_frame1 = int(cols/2)
+    # cenY_frame1 = int(rows/2)
+
+    _, frame2 = OMNI_CAP.read()
+    rows1, cols1, _ = frame2.shape
+    cenX_frame2 = int(cols1/2)
+    cenY_frame2 = int(rows1/2)
     
-    inner_left = cenX_frame1 - 100
-    outer_left = cenX_frame1 - 250 
-    inner_right = cenX_frame1 + 100
-    outer_right = cenX_frame1 + 250
-    inner_top = cenY_frame1 - 100
-    outer_top = cenY_frame1 - 150
-    inner_bottom = cenY_frame1 + 100
-    outer_bottom = cenY_frame1 + 150
+    inner_left = cenX_frame2 - 120
+    outer_left = cenX_frame2 - 170 
+    inner_right = cenX_frame2 + 55
+    outer_right = cenX_frame2 + 105
+    inner_top = cenY_frame2 - 100
+    outer_top = cenY_frame2 - 120
+    inner_bottom = cenY_frame2 + 55
+    outer_bottom = cenY_frame2 + 80
 
     # read magenta color
-    objColor = getBallInfo()
+    objColor = getBallInfo2()
     lowerBall = np.array([objColor[0],objColor[1],objColor[2]])
     upperBall = np.array([objColor[3],objColor[4],objColor[5]])
 
     dari = ""
-    second = 0
+
     startCount = 10
     count = startCount
-    speed = 150
+    speed = 35
     state = "START"
 
     dribbling(db,1)
-    db.flush()
-
-    setMotor(motor, 0,speed,-speed,0) # serong kiri
+    #db.flush()
+    dummy1, dummy2, kiper, mode, gameStatus = getGameInfo()
 
     while(True):
-        dummy1, dummy2, kiper, mode, gameStatus = getGameInfo()
         if gameStatus == "RETRY":
+            cv2.destroyAllWindows()
             break
         if count <= 0:
             motor.close()
+            dummy1, dummy2, kiper, mode, gameStatus = getGameInfo()
             count = startCount
         count -= 1
         #print(state)
-        second += 1
-        #print(second)
         for i in range(3):
-            FRONT_CAP.grab()
+            #FRONT_CAP.grab()
+            OMNI_CAP.grab()
         ## read frame
-        _, frame1 = FRONT_CAP.read()
+        #_, frame1 = FRONT_CAP.read()
+        _, frame2 = OMNI_CAP.read()
         
         # convert frame from BGR to HSV
-        hsv = cv2.cvtColor(frame1, cv2.COLOR_BGR2HSV)
+        #hsv = cv2.cvtColor(frame1, cv2.COLOR_BGR2HSV)
+        hsv1 = cv2.cvtColor(frame2, cv2.COLOR_BGR2HSV)
 
         # blur the frame
-        blur = cv2.medianBlur(hsv, 5)
+        #blur = cv2.medianBlur(hsv, 5)
+        blur1 = cv2.medianBlur(hsv1, 5)
 
         # create a mask from blurred frame
-        BALL_MASK = cv2.inRange(blur, lowerBall, upperBall)
+        #BALL_MASK = cv2.inRange(blur, lowerBall, upperBall)
+        BALL_MASK1 = cv2.inRange(blur1, lowerBall, upperBall)
 
         # convert to black and white image
-        _, BALL_THRESH = cv2.threshold(BALL_MASK, objColor[6], 255, 0)
+        #_, BALL_THRESH = cv2.threshold(BALL_MASK, objColor[6], 255, 0)
+        _, BALL_THRESH1 = cv2.threshold(BALL_MASK1, objColor[6], 255, 0)
 
         # refine the image using morphological transformation
         kernal = np.ones((5,5), np.uint8)
-        BALL_MORPH = cv2.morphologyEx(BALL_THRESH, cv2.MORPH_CLOSE, kernal, iterations = 2)
+        #BALL_MORPH = cv2.morphologyEx(BALL_THRESH, cv2.MORPH_CLOSE, kernal, iterations = 2)
+        BALL_MORPH1 = cv2.morphologyEx(BALL_THRESH1, cv2.MORPH_CLOSE, kernal, iterations = 2)
 
         # find contours
-        ballContours, _ = cv2.findContours(BALL_MORPH, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        ballContours = sorted(ballContours, key=lambda x:cv2.contourArea(x), reverse=True)
-
-        if state == "FINISH":
-            setMotor(motor,0,0,0,0)
-            motor.close()
-            cv2.destroyAllWindows()
-            break
+        #ballContours, _ = cv2.findContours(BALL_MORPH, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        #ballContours = sorted(ballContours, key=lambda x:cv2.contourArea(x), reverse=True)
+        
+        ballContours1, _ = cv2.findContours(BALL_MORPH1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        ballContours1 = sorted(ballContours1, key=lambda x:cv2.contourArea(x), reverse=True)
 
         if db.isOpen() == False:
             db.open()
         reading = db.readline().decode('utf-8','ignore')
+        # db.reset_input_buffer()
         if len(reading) > 0 :
             head = reading[0:5]
-            if  head == "Tidak" :
-                print("TIDAK DAPAT BOLA")
-                arahBolaDepan()
-                break    
-         
+            print(head)
+            if  head == "Dapat" :
+                print("DAPAT BOLA")
+                state = "FINISH"    
+
+        # if stm.isOpen() == False:
+        #     stm.open()
+        # reading = stm.readline().decode('utf-8','ignore')
+        # # db.reset_input_buffer()
+        # if len(reading) > 0 :
+        #     head = reading[0:5]
+        #     print(head)
+        #     if  head == "Dapat" :
+        #         print("DAPAT BOLA")
+        #         state = "FINISH"    
+
         pas = 0
 
-        if state == "FINISH"  or second > 25: 
-            setMotor(motor,0,0,0,0)
-            break
-
-        for ballContour in ballContours:
+        for ballContour in ballContours1:
             ball_area = cv2.contourArea(ballContour)
-            if ball_area > 500:
+            if ball_area > 10:
                 (x_ball, y_ball, w_ball, h_ball) = cv2.boundingRect(ballContour)
-                cv2.putText(frame1, "X: "+str(x_ball)+" Y: "+str(y_ball), (520, 20), font, 0.5, (0,0,255),2)
+                cv2.putText(frame2, "X: "+str(x_ball)+" Y: "+str(y_ball), (20, 20), font, 0.5, (0,0,255),2)
                 cenX_ball = (x_ball+x_ball+w_ball)/2
                 cenY_ball = (y_ball+y_ball+h_ball)/2   
-                print(cenX_ball)
+                #print("X: "+str(cenX_ball)+" Y: "+str(cenY_ball))
                 # draw actual coordinate from segmentation
-                cv2.circle(frame1, (int(cenX_ball), int(cenY_ball)), 20, [0,255,0], 2, 8)
-                cv2.line(frame1, (int(cenX_ball), int(cenY_ball + 20)), (int(cenX_ball + 50), int(cenY_ball + 20)), [0,255,0], 2, 8)
-                cv2.putText(frame1, "Actual", (int(cenX_ball + 50), int(cenY_ball + 20)), font, 0.5, [0,255,0], 2)
+                cv2.circle(frame2, (int(cenX_ball), int(cenY_ball)), 20, [0,255,0], 2, 8)
+                cv2.line(frame2, (int(cenX_ball), int(cenY_ball + 20)), (int(cenX_ball + 50), int(cenY_ball + 20)), [0,255,0], 2, 8)
+                cv2.putText(frame2, "Actual", (int(cenX_ball + 50), int(cenY_ball + 20)), font, 0.5, [0,255,0], 2)
                 
-                if cenX_ball > inner_left-20:
-                    setMotor(motor,0,-50,50,0)
-                    sleep(0.1)    
-                    setMotor(motor,0,0,0,0)
-                    state = "FINISH"   
+                if cenX_ball > 200:
+                    setMotor(motor, 0,0,0,0)
+                    #pas = 1
+                    #state = "FINISH"
+                    setStatus(2, "READY")
+                break
+        
+        if state == "FINISH": 
+            setMotor(motor,0,0,0,0)
+            motor.close()
+            cv2.destroyAllWindows()
+            break        
 
         # displays
         ## uncomment this to show center area of the frame 1
-        cv2.rectangle(frame1, (inner_left, inner_top), (inner_right, inner_bottom), (0,255,0), 2)
-        cv2.rectangle(frame1, (outer_left, outer_top), (outer_right, outer_bottom), (0,255,255), 2)
+        cv2.rectangle(frame2, (inner_left, inner_top), (inner_right, inner_bottom), (0,255,0), 2)
+        #cv2.rectangle(frame2, (outer_left, outer_top), (outer_right, outer_bottom), (0,255,255), 2)
         #cv2.rectangle(frame2, (xAwal, yAwal), (xAkhir, yAkhir), (0,255,0), 2)
 
-        cv2.imshow("Kamera Depan", frame1)
-        cv2.moveWindow("Kamera Depan" ,20,20)
+        #cv2.imshow("Kamera Depan", frame1)
+        #cv2.moveWindow("Kamera Depan" ,20,20)
+        
+        cv2.imshow("Kamera Atas", frame2)
+        cv2.moveWindow("Kamera Atas" ,0,0)
         
         #print(ballColor)
         
@@ -1006,9 +1032,11 @@ def mulaiSerongKiri():
                 motor.open()
             motor.write(b"#M|STP|0\n")
             db.write(b"DB OFF")
-            FRONT_CAP.release()
+            #FRONT_CAP.release()
+            OMNI_CAP.release()
             cv2.destroyAllWindows()
             break
+    return gameStatus
 
 def tesLimit():
     print("==>> Tes Limit")
@@ -1571,7 +1599,15 @@ def main():
 
             elif mode == "KICKOFF KIRI":
                 while gameStatus == "START":
-                    serongKiri(170, 3)
+                    # serongKiri(170, 3)
+                    gameStatus = mulaiSerongKiri()
+                    if gameStatus == "RETRY":
+                        dribbling(db,0)
+                        putarDerajat(86,0)
+                        mundur(90, 1)
+                        mundurSerongKanan(180, 2)
+                        #setGame("STOP")
+                        break
                     gameStatus = lurusBolaAtas()
                     if gameStatus == "RETRY":
                         dribbling(db,0)
