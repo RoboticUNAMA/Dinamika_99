2635c6ed4e2a2599afcceda3ff22f4eb8e22bd11
Arjuna Panji <arjuna.panji97@gmail.com>
2021-09-15 16:02:09 +0700

    simulasi

--- app/main.py
+++ app/main.py
@@ -633,7 +633,7 @@ def arahRobotDepan():
                     
                 elif cenX_ball > 0 and cenX_ball < 200  :
                     setMotor(motor,50,50,50,50)
-                    sleep(0.115)
+                    sleep(0.1)
                     setMotor(motor,0,0,0,0)
                     sleep(0.1)
                     dari = "kanan"
@@ -642,7 +642,7 @@ def arahRobotDepan():
                 
                 elif cenX_ball > 210 :
                     setMotor(motor,-50,-50,-50,-50)
-                    sleep(0.115)
+                    sleep(0.1)
                     setMotor(motor,0,0,0,0)
                     sleep(0.1)
                     dari = "kiri"
