08bde49c3abbf23ceb33a4704029984b24918aeb
Arjuna Panji <arjuna.panji97@gmail.com>
2021-09-18 20:45:37 +0700

    arahkiper

--- app/main.py
+++ app/main.py
@@ -1032,13 +1032,19 @@ def arahKiperKanan():
             cv2.line(frame1, (int(cenX_ball), int(cenY_ball + 20)), (int(cenX_ball + 50), int(cenY_ball + 20)), [0,255,0], 2, 8)
             cv2.putText(frame1, "Actual", (int(cenX_ball + 50), int(cenY_ball + 20)), font, 0.5, [0,255,0], 2)
             
-            if cenX_ball < 130  :
-                setMotor(motor,35,35,35,35)
+            if cenX_ball < 100+30  :
+                setMotor(motor,40,40,40,40)
+                sleep(0.2)
+                setMotor(motor,0,0,0,0)
+                sleep(0.1)
                 
-            elif cenX_ball > 330  :
-                setMotor(motor,-35,-35,-35,-35)
+            elif cenX_ball > 300+30  :
+                setMotor(motor,-40,-40,-40,-40)
+                sleep(0.2)
+                setMotor(motor,0,0,0,0)
+                sleep(0.1)
                 
-            elif cenX_ball < 210  :
+            elif cenX_ball < 180+30  :
                 setMotor(motor,40,40,40,40)
                 sleep(0.1)
                 setMotor(motor,0,0,0,0)
@@ -1046,7 +1052,7 @@ def arahKiperKanan():
                 dari = "kanan"
                 print("PUTAR KANAN")
             
-            elif cenX_ball > 250 :
+            elif cenX_ball > 220+30 :
                 setMotor(motor,-40,-40,-40,-40)
                 sleep(0.1)
                 setMotor(motor,0,0,0,0)
@@ -1054,7 +1060,7 @@ def arahKiperKanan():
                 dari = "kiri"
                 print("PUTAR KIRI")
             else:
-                if count2 > 7:
+                if count2 > 5:
                     setMotor(motor,0,0,0,0)
                     state = "FINISH"
 
@@ -1223,10 +1229,16 @@ def arahKiperKiri():
             cv2.putText(frame1, "Actual", (int(cenX_ball + 50), int(cenY_ball + 20)), font, 0.5, [0,255,0], 2)
             
             if cenX_ball < 100-30  :
-                setMotor(motor,35,35,35,35)
+                setMotor(motor,40,40,40,40)
+                sleep(0.2)
+                setMotor(motor,0,0,0,0)
+                sleep(0.1)
                 
             elif cenX_ball > 300-30  :
-                setMotor(motor,-35,-35,-35,-35)
+                setMotor(motor,-40,-40,-40,-40)
+                sleep(0.2)
+                setMotor(motor,0,0,0,0)
+                sleep(0.1)
                 
             elif cenX_ball < 180-30  :
                 setMotor(motor,40,40,40,40)
