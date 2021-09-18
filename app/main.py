9581b0f7dfc46370ee3f30685f7c7a6f8bb6a637
Arjuna Panji <arjuna.panji97@gmail.com>
2021-09-18 20:51:18 +0700

    arahkiper

--- app/main.py
+++ app/main.py
@@ -1032,19 +1032,19 @@ def arahKiperKanan():
             cv2.line(frame1, (int(cenX_ball), int(cenY_ball + 20)), (int(cenX_ball + 50), int(cenY_ball + 20)), [0,255,0], 2, 8)
             cv2.putText(frame1, "Actual", (int(cenX_ball + 50), int(cenY_ball + 20)), font, 0.5, [0,255,0], 2)
             
-            if cenX_ball < 100+45  :
+            if cenX_ball < 100+60:
                 setMotor(motor,40,40,40,40)
                 sleep(0.2)
                 setMotor(motor,0,0,0,0)
                 sleep(0.1)
                 
-            elif cenX_ball > 300+45  :
+            elif cenX_ball > 300+60:
                 setMotor(motor,-40,-40,-40,-40)
                 sleep(0.2)
                 setMotor(motor,0,0,0,0)
                 sleep(0.1)
                 
-            elif cenX_ball < 180+45  :
+            elif cenX_ball < 180+60:
                 setMotor(motor,40,40,40,40)
                 sleep(0.1)
                 setMotor(motor,0,0,0,0)
@@ -1052,7 +1052,7 @@ def arahKiperKanan():
                 dari = "kanan"
                 print("PUTAR KANAN")
             
-            elif cenX_ball > 220+45 :
+            elif cenX_ball > 220+60:
                 setMotor(motor,-40,-40,-40,-40)
                 sleep(0.1)
                 setMotor(motor,0,0,0,0)
