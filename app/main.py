82b09827d013e1f1baa4ceac854c3aee99ecad68
Arjuna Panji <arjuna.panji97@gmail.com>
2021-09-22 16:58:41 +0700

    mode1

--- app/main.py
+++ app/main.py
@@ -2623,7 +2623,7 @@ def main():
                         mundur(90, 1.2)
                         #setGame("STOP")
                         break
-                    sleep(2)
+                    sleep(3)
                     maju(90,1)
                     putarKanan(90, 0.3)
                     gameStatus = arahRobotDepan()
