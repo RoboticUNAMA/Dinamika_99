1e4cabef3eb0d16e2d44c488c5a1f104c4ba3607
Arjuna Panji <arjuna.panji97@gmail.com>
2021-09-23 23:12:43 +0700

    a

--- app/main.py
+++ app/main.py
@@ -2691,7 +2691,7 @@ def main():
                         setMotor(motor, 0,0,0,0)
                         if getStatus(1) == "READY":
                             break
-                    sleep(1)
+                    putarKiri(40,0.1)
                     oper(db)
                     setStatus(2, "RUNNING")
                     mundurSerongKanan(120, 1.5)
