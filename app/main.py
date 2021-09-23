bd0427fe3c6536d710b569d18c7f3146a87a6f2b
Arjuna Panji <arjuna.panji97@gmail.com>
2021-09-24 03:37:15 +0700

    a

--- app/main.py
+++ app/main.py
@@ -2590,7 +2590,7 @@ def main():
             if mode == "KICKOFF KANAN":
                 while gameStatus == "START":
                     setStatus(2, "RUNNING")
-                    maju(90, 0.7)
+                    maju(90, 1)
                     serongKiri(180, 1.5)
                     putarKiri(90, 0.25)
                     arahBolaDepan()
