fbe90f2606d987fc5f29a014ef6bbcc8826632fb
Arjuna Panji <arjuna.panji97@gmail.com>
2021-09-23 17:56:48 +0700

    a

--- app/main.py
+++ app/main.py
@@ -475,13 +475,13 @@ def arahBolaDepan():
                     
                     
                 elif cenX_ball < 180  :
-                    putarKiri(35, 0.15)
+                    putarKiri(50, 0.15)
                     dari = "kanan"
                     setStatus(2, "LURUS BOLA")
                     print("PUTAR KANAN")
                 
                 elif cenX_ball > 220 :
-                    putarKanan(35, 0.15)
+                    putarKanan(50, 0.15)
                     dari = "kiri"
                     setStatus(2, "LURUS BOLA")
                     print("PUTAR KIRI")
