83374be4ce7c1b0a2b699efacbf1f51021f214f2
Arjuna Panji <arjuna.panji97@gmail.com>
2021-09-24 01:05:57 +0700

    a

--- app/main.py
+++ app/main.py
@@ -2768,7 +2768,7 @@ def main():
                 while gameStatus == "START":
                     setStatus(2, "RUNNING")
                     serongKiri(150,1.2)
-                    maju(90,1.5)
+                    maju(90,1.4)
                     putarKiri(90,0.3)
                     arahBolaDepan()
                     setStatus(2, "RUNNING")
