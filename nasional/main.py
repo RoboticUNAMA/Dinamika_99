2cf9f2d332a9dc720c2e1bf67d0b25c4f912f3bf
Arjuna Panji <arjuna.panji97@gmail.com>
2021-10-08 02:39:22 +0700

    update nasional

--- nasional/main.py
+++ nasional/main.py
@@ -6,9 +6,7 @@ def main():
         game = getGame()
         print(game)
         if game == "START":
-            db_on(db)
-            sleep(3)
-            db_off(db)
+            tendang(db)
             break
 
 if __name__ == '__main__':
