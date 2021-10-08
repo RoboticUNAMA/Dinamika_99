d45808c063ac000cc03fe92e781224f350ac5f7b
Arjuna Panji <arjuna.panji97@gmail.com>
2021-10-08 23:05:18 +0700

    update

--- app/main.py
+++ app/main.py
@@ -273,11 +273,11 @@ def read_from_port(ser,que_output):
 def mulaiKickoff():
     maju(90,0.3)
     geserKiri(90, 0)
-    sleep(0.3)
+    sleep(0.5)
     geserKiri(255, 1.5)
     maju(90,0)
-    sleep(0.3)
-    maju(255, 1.2)
+    sleep(0.5)
+    maju(255, 1.3)
     putarKanan(90,0.3)
     lurusBolaAtas()
 
