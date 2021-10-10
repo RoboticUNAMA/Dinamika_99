a5a2ada788798030a82dde57a34df396d6a98ae5
Arjuna Panji <arjuna.panji97@gmail.com>
2021-10-10 21:27:56 +0700

    Update Arah Bola

--- app/main.py
+++ app/main.py
@@ -75,7 +75,6 @@ def setMotor(ser,dki,dka,bki,bka) :
     if ser.isOpen() == False:
         ser.open()
     ser.write(("#M|RUN|0|0|0|0\n").encode('utf-8'))
-    sleep(0.1)
     dki = dki + (dki * 0.3)
     dka = dka + (dka * 0)
     bki = bki + (bki * 0)
