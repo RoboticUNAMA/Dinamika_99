d24dba2617e55891d3103a00c2926903dced602d
Arjuna Panji <arjuna.panji97@gmail.com>
2021-10-11 17:10:28 +0700

    Update Arah Bola

--- app/main.py
+++ app/main.py
@@ -2513,7 +2513,7 @@ def arahBolaAtas():
                 cv2.line(frame2, (int(cenX_ball), int(cenY_ball + 20)), (int(cenX_ball + 50), int(cenY_ball + 20)), [0,255,0], 2, 8)
                 cv2.putText(frame2, "Actual", (int(cenX_ball + 50), int(cenY_ball + 20)), font, 0.5, [0,255,0], 2)
                 
-                cenX = 140
+                cenX = 240
                 cenY = 60
 
                 if cenX_ball < cenX and cenY_ball > cenY + 20:
