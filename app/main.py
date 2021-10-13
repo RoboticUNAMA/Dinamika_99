7a25d9666f7256eded3bca3b712ec4c603f1b9c5
Arjuna Panji <arjuna.panji97@gmail.com>
2021-10-13 23:10:33 +0700

    update nasional

--- app/main.py
+++ app/main.py
@@ -2354,7 +2354,7 @@ def lurusBolaAtas():
                 cv2.line(frame2, (int(cenX_ball), int(cenY_ball + 20)), (int(cenX_ball + 50), int(cenY_ball + 20)), [0,255,0], 2, 8)
                 cv2.putText(frame2, "Actual", (int(cenX_ball + 50), int(cenY_ball + 20)), font, 0.5, [0,255,0], 2)
                 
-                cenX = 158
+                cenX = 150
                 cenY = 100
 
                 if cenX_ball < cenX-30  and cenY_ball < cenY :
