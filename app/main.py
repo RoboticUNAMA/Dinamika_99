84cf8ce506b69c8aec34f749ebcf687648d15b49
Arjuna Panji <arjuna.panji97@gmail.com>
2021-10-13 23:12:04 +0700

    update nasional

--- app/main.py
+++ app/main.py
@@ -2354,7 +2354,7 @@ def lurusBolaAtas():
                 cv2.line(frame2, (int(cenX_ball), int(cenY_ball + 20)), (int(cenX_ball + 50), int(cenY_ball + 20)), [0,255,0], 2, 8)
                 cv2.putText(frame2, "Actual", (int(cenX_ball + 50), int(cenY_ball + 20)), font, 0.5, [0,255,0], 2)
                 
-                cenX = 150
+                cenX = 148
                 cenY = 100
 
                 if cenX_ball < cenX-30  and cenY_ball < cenY :
@@ -2365,11 +2365,11 @@ def lurusBolaAtas():
                     print("PUTAR KIRI JAUH")
                     putarKiri(speed, 0)
                     
-                elif cenX_ball < cenX-5 and cenY_ball < cenY :
+                elif cenX_ball < cenX-3 and cenY_ball < cenY :
                     print("PUTAR KANAN DEKAT")
                     putarKanan(speed, 0.15)
                 
-                elif cenX_ball > cenX+5 and cenY_ball < cenY :
+                elif cenX_ball > cenX+3 and cenY_ball < cenY :
                     print("PUTAR KIRI DEKAT")
                     putarKiri(speed, 0.15)
                 else :
