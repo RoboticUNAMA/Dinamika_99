26a564102fde5395f311d2f052d80bc4f0979ce8
Arjuna Panji <arjuna.panji97@gmail.com>
2021-10-15 01:56:53 +0700

    update

--- app/main.py
+++ app/main.py
@@ -2993,7 +2993,8 @@ def main():
                 putarKanan(90,0.3)
                 arahBolaAtas()
                 if kiper != "1":
-                    putarKiri(90,0.8)
+                    putarKiri(90,0.9)
+                    maju(120,0.6)
                 else:
                     putarKiri(90,0.6)
                     #arahKiper()
